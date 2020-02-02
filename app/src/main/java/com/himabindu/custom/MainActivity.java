7e0f4a3abd077a04109d704b1e2f722c5dd788c8
HimabinduRavula <himabindu2v@gmail.com>
2020-02-03 00:19:24 +0530

    first

--- app/src/main/java/com/himabindu/custom/MainActivity.java
+++ app/src/main/java/com/himabindu/custom/MainActivity.java
@@ -1,13 +1,23 @@
-package com.himabindu.custom;
+    package com.himabindu.custom;
 
 import android.hardware.Camera;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     Camera camera;
+    Button b1;
+    Button  b2;
     FrameLayout frameLayout;
     ShowCamera showCamera;
     @Override
@@ -15,13 +25,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         frameLayout =(FrameLayout)findViewById(R.id.framelayout);
-
-
+        b1=findViewById(R.id.button1);
+        b2=findViewById(R.id.button2);
         camera= Camera.open();
 
         showCamera=new ShowCamera(this,camera);
+        frameLayout.addView(showCamera);
 
 
 
+    }
+    Camera.PictureCallback mPictureCallback=new Camera.PictureCallback() {
+         @Override
+        public void  onPictureTaken(byte[] data, Camera camera){
+             File picture_file= getOutputMediaFile();
+             if (picture_file==null)
+             {
+                 return;
+             }
+             else {
+                 try {
+
+
+                     FileOutputStream fos = new FileOutputStream(picture_file);
+                     fos.write(data);
+                     fos.close();
+                     camera.startPreview();
+                 }
+                 catch (IOException e)
+                 {
+                     e.printStackTrace();
+                 }
+             }
+         }
+    };
+
+    private File getOutputMediaFile() {
+    String  state= Environment.getExternalStorageState();
+    if (!state.equals(Environment.MEDIA_MOUNTED)){
+        return null;
+    }
+    else
+    {
+        File folder_gui=new File(Environment.getExternalStorageDirectory()+ File.separator
+        + "GUI");
+        if (!folder_gui.exists()){
+            folder_gui.mkdirs();
+        }
+        File outputFile=new File(folder_gui,"temp.jpg");
+        return outputFile;
+    }
+    }
+
+
+    public void captureImage(View v){
+      if (camera!=null)
+      {
+          camera.takePicture(null,null,mPictureCallback);
+      }
     }
 }
--- app/src/main/java/com/himabindu/custom/ShowCamera.java
+++ app/src/main/java/com/himabindu/custom/ShowCamera.java
@@ -7,6 +7,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
     Camera camera;
@@ -21,6 +22,14 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
        Camera.Parameters params=camera.getParameters();
+
+       List<Camera.Size> sizes=params.getSupportedPictureSizes();
+       Camera.Size msize=null;
+       for (Camera.Size size:sizes)
+       {
+           msize=size;
+       }
+
        if (this.getResources().getConfiguration().orientation!= Configuration.ORIENTATION_LANDSCAPE){
            params.set("orientation","portrait");
            camera.setDisplayOrientation(90);
@@ -33,7 +42,7 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
            camera.setDisplayOrientation(0);
            params.setRotation(0);
        }
-
+          params.setPictureSize(msize.width,msize.height);
 
        camera.setParameters(params);
        try {
@@ -54,6 +63,7 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+         camera.stopPreview();
+         camera.release();
     }
 }
